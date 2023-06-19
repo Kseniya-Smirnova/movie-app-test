3714ac78b16289f3b4334c252fd876087cf98046
Kseniya Smirnova <Kseniya_Smirnova@epam.com>
2023-06-19 17:02:20 +0200

    Update run-script.js

--- run-script.js
+++ run-script.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-fs.appendFileSync('./message.txt', 'data to append');
+fs.appendFileSync('src/message.txt', 'data to append');
 
-let fileContent = fs.readFileSync('./message.txt', 'utf8');
+let fileContent = fs.readFileSync('src/message.txt', 'utf8');
 console.log(fileContent);
