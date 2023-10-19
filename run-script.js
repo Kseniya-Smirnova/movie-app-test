5ce36697a7478b24a3f3309f61428f1f9c29ca89
Kseniya Smirnova <Kseniya_Smirnova@epam.com>
2023-10-19 13:03:30 +0200

    Update run-script.js

--- run-script.js
+++ run-script.js
@@ -1,11 +1,24 @@
 const fs = require('fs');
 const { exec } = require('child_process');
 
-exec(
-    `
-       node_modules/.bin/openapi-generator-cli generate -i https://raw.githubusercontent.com/OpenAPITools/openapi-generator/master/modules/openapi-generator/src/test/resources/3_0/petstore.yaml -g typescript-axios -o ./test --additional-properties=withSeparateModelsAndApi=true,modelPackage=models,apiPackage=api,prependFormOrBodyParameters=true --skip-validate-spec 
-          --skip-validate-spec
+function readDir(dirPath) {
+    console.log(dirPath);
+    fs.readdir(`${dirPath}`, (err, files) => {
+        files.forEach(file => {
+            console.log('Files: ', file);
+        });
+    });
+}
+
+function run() {
+    exec(
+        `
+       openapi-generator-cli generate -i https://raw.githubusercontent.com/OpenAPITools/openapi-generator/master/modules/openapi-generator/src/test/resources/3_0/petstore.yaml -g typescript-axios -o ./test
     `, {
-        cwd: process.cwd()
-    }
-);
+            cwd: process.cwd()
+        }
+    );
+    readDir('./test', process.cwd());
+}
+
+run();
